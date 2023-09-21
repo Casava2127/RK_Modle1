1abd924701b4a77fb3ad82bc3a1dca4f3db3cd73
Casava2127 <rum01285800687@gmail.com>
2023-09-21 15:47:04 +0700

    2109

---
+++ SS9-Demo/app.js
@@ -0,0 +1,61 @@
+
+// var name=9=============
+// alert("Hello 500 anh em - cách 2"+" "+name)
+// var: variable: Từ khóa khai báo biến
+// comment theo dòng ==============
+// comment theo khối /**  */
+
+// các kiểu dữ liệu==================
+// String: dạng chuỗi => được đọc trong cặp ngoặc đơn v kép
+// Number
+// Boolean
+// Object
+// null
+// undefined : dạng giá trị không xác định
+
+
+// quy tắc khai báo biến===============
+//  có 3 từ khóa khai báo biến: var let const
+/**
+ * var: có thể thay đổi giá trị của biến và khai báo lại biến đó- biến global
+ * let: ó thể thay đổi giá reij của biến và không thể khai báo lại biến - biến cục bộ
+ * const: biến hằng
+ */
+// var name= "Quantv"
+// var name= "RUM"
+// alert("Hello 500 anh em - cách 2"+" "+name)
+
+// let name1 = "rum1"
+//    name1= "rum2"
+//    alert("Heello 500 ae"+ name1)
+
+//    quy tắc đặt tên biến==============
+/** khi đặt tên biến thì có 3 cách đặt tên biến camelCase, kebab-case, snake_case
+ * came: viết thường chữ cái đầu, viết hoa chữ cái đầu tiến tiếp theo
+ * keb: viết thường tất cả chữ cái và cách nhau một dấu gạch ngang -
+ * snake: viết thường tất cả chữ cái và cách nhau một dấu gạch ngang _
+ */
+
+
+// Toán tử=======
+
+
+// Phương thức WINDOW
+
+
+// console.log(confirm("NguyenQUan"))
+// // Hiển thị hộp thông báo có nội dung message và nút OK, Cancel
+// // ■ Trả về true nếu chọn OK, ngược lại là false
+
+// console.log(prompt("ok thì ra default text ","mặc định"))
+// // Nhấn ok thì ra defaule text - còn không thì NULL 
+
+// const myName = prompt("Quan ơi")
+// console.log(myName);
+
+// alert("me keu"+myName)
+
+
+// toán tử gán =======
+// toán tử so sánh
+// == === != !==
