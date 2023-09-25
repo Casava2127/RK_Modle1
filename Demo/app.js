8d7a42c4eacbe7aab0e4508d0c97dc637b761e6a
Casava2127 <rum01285800687@gmail.com>
2023-09-25 18:41:43 +0700

    2509

---
+++ Demo/app.js
@@ -0,0 +1,20 @@
+
+// function getdate() {
+//   document.getElementById('demo').innerHTML = Date();
+// }
+// // Luyện tập if else 
+// var input = parseInt(prompt("Nhập vào 1 số: "))
+// if (input > 7 || input < 0) {
+//   alert("Nhập không hợp lệ")
+// } else if (input == 7) {
+//   alert("Hôm nay là Chủ Nhật")
+// } else {
+//   alert("Hôm nay là thứ " + input)
+// }
+
+
+let check =1;
+while(check<4){
+  console.log(check)
+  check++;
+}
---
+++ ThucHanh/js2.js
@@ -0,0 +1,13 @@
+
+// Bai1
+// var a = parseInt(prompt("Enter a"))
+// var b = parseInt(prompt("Enter b"))
+// kq = (a+b) <4 ? 'Below' : 'Over'
+// alert(kq)
+
+//Bai2
+//  let login = prompt("Nhập chuỗi");
+//  let mes = (login =='Employee') ?  alert('Hello'): ((login=='Director') ? alert('Greetings') : ((login=='') ? alert('No login') : alert('')))
+
+
+// Bai3
